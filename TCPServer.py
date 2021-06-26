5458eea2eef17c453e235494d23fe56b0a3e74ba
shiyunlian <shiyunlian00@gmail.com>
2021-06-25 21:53:40 -0700

    update server

--- TCPServer.py
+++ TCPServer.py
@@ -1,7 +1,14 @@
 from socket import *
 serverPort = 12000
+
+# creates the server’s socket
 serverSocket = socket(AF_INET,SOCK_STREAM)
+
+#associate the server port number with server socketß
 serverSocket.bind(('',serverPort))
+
+#the server listen for TCP connection requests from the client. 
+# The parameter specifies the maximum number of queued connections (at least 1).
 serverSocket.listen(1)
 print('The server is ready to receive')
 
