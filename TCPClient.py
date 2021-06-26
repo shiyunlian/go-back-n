9738c36e0d51ea31756d4f8cad21d485a459750f
shiyunlian <shiyunlian00@gmail.com>
2021-06-25 21:53:23 -0700

    update client

--- TCPClient.py
+++ TCPClient.py
@@ -1,12 +1,26 @@
 from socket import *
 # serverName = ’servername’
 # serverName='172.16.210.4'
-serverName='10.0.0.81'
+# serverName='10.0.0.175'
+serverName='127.0.0.1'
 serverPort = 12000
+
+# creates the client’s socket,the first parameter indicates the underlying network is using IPv4. 
+# The second parameter indicates that the socket is a TCP socket
 clientSocket = socket(AF_INET, SOCK_STREAM)
+
+# initiates the TCP connection between the client and server.
 clientSocket.connect((serverName,serverPort))
+
+#obtains a sentence from the user.
 sentence = input('Input lowercase sentence:')
+
+#sends the sentence through the client’s socket and into the TCP connection.
 clientSocket.send(sentence.encode())
+
 modifiedSentence = clientSocket.recv(1024)
+
 print('From Server:' , modifiedSentence.decode())
+
+#closes the socket and closes the TCP connection
 clientSocket.close()
