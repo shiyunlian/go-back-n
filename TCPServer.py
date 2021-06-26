244c2ec5fc20f14e84962e82133dbc05ce640581
shiyunlian <shiyunlian00@gmail.com>
2021-06-26 14:20:04 -0700

    update server

--- TCPServer.py
+++ TCPServer.py
@@ -4,17 +4,16 @@ serverPort = 12000
 # creates the server’s socket
 serverSocket = socket(AF_INET,SOCK_STREAM)
 
-#associate the server port number with server socketß
+# associate the server port number with server socket
 serverSocket.bind(('',serverPort))
 
-#the server listen for TCP connection requests from the client. 
+# the server listen for TCP connection requests from the client. 
 # The parameter specifies the maximum number of queued connections (at least 1).
 serverSocket.listen(1)
 print('The server is ready to receive')
 
 while True:
     connectionSocket, addr = serverSocket.accept()
-    sentence = connectionSocket.recv(1024).decode()
-    capitalizedSentence = sentence.upper()
-    connectionSocket.send(capitalizedSentence.encode())
+    response = 'success' if connectionSocket.recv(1024).decode() == 'network' else 'failure'
+    connectionSocket.send(response.encode())
     connectionSocket.close()
