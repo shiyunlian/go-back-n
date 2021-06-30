fce943fb9688d790424906f6ee123cd2c882ffe1
shiyunlian <shiyunlian00@gmail.com>
2021-06-29 22:27:54 -0700

    update part c

--- Part_C.py
+++ Part_C.py
@@ -1,13 +1,11 @@
 from socket import *
 serverPort = 1234
-
-# creates the server’s socket
 serverSocket = socket(AF_INET,SOCK_STREAM)
-
-# associate the server port number with server socket
 serverSocket.bind(('',serverPort))
 
-# the server listen for TCP connection requests from the client
-# the parameter specifies the maximum number of queued connections (at least 1)
-serverSocket.listen(1)
-print('The server is ready to receive')
+try:
+    serverSocket.listen(10)
+    print('success')
+except:
+    print('failure')
+    
