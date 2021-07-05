971c5e662110def618d97647e59c923d79085c36
shiyunlian <shiyunlian00@gmail.com>
2021-07-04 23:14:13 -0700

    adjust server and client codes

--- TCPClient.py
+++ TCPClient.py
@@ -1,12 +1,11 @@
-from socket import *
-# serverName = ’servername’
-serverName='172.16.210.4'
+import time, socket
+
+# serverName='172.16.210.4'
 # serverName='10.0.0.175'
 # serverName='127.0.0.1'
-serverPort = 12000
+serverName = '10.0.0.81'
+serverPort = 1234
 
-# creates the client’s socket,the first parameter indicates the underlying network is using IPv4. 
-# The second parameter indicates that the socket is a TCP socket
 clientSocket = socket(AF_INET, SOCK_STREAM)
 
 # initiates the TCP connection between the client and server.
@@ -19,6 +18,58 @@ clientSocket.send(request.encode())
 # receive response from server
 response = clientSocket.recv(1024)
 print('From Server:' , response.decode())
+
+while True:
+    message = input(str("Please enter the bits of sequence number or 'exit' to exit the program: "))
+    if message == "exit":
+        conn.send(message.encode())
+        print("Program terminated.")
+        break
+    conn.send(message.encode())
+    seq_num_range=pow(2,int(message))-1
+    print("The range of sequence number is 0 to", seq_num_range)
+    
+    n=0
+    message_list = ""
+    while n < seq_num_range:
+        message_list= message_list+str(n)
+        n+=1
+    
+    win_size=int(input("Please enter the window size: "))
+    win_size=win_size-1
+    win_begin=0
+    win_end=win_size
+    ack_message=""
+    
+    while win_begin!=seq_num_range:
+        while(win_begin!=(seq_num_range-win_size)):
+            conn.send(message_list[win_begin].encode())
+            ack_message=conn.recv(1024).decode()
+            print(ack_message)
+            if(ack_message!="ACK Lost"):
+                time.sleep(1)
+                print("Acknowledgement received! The sliding window is in the range "+(str(win_begin+1))+" to "+str(win_end+1)+". Send the next sequence number.")
+                win_begin=win_begin+1
+                win_end=win_end+1
+                time.sleep(1)
+            else:
+                time.sleep(1)
+                print("Acknowledgement lost! The sliding window remains in the range "+(str(win_begin+1))+" to "+str(win_end+1)+". Resend the same sequence number.")
+                time.sleep(1)
+
+        while(win_begin!=seq_num_range):
+            conn.send(message_list[win_begin].encode())
+            ack_message=conn.recv(1024).decode()
+            print(ack_message)
+            if(ack_message!="ACK Lost"):
+                time.sleep(1)
+                print("Acknowledgement received! The sliding window is in the range "+(str(win_begin+1))+" to "+str(win_end)+". Send the next sequence number.")
+                win_begin=win_begin+1
+                time.sleep(1)
+            else:
+                time.sleep(1)
+                print("Acknowledgement lost! The sliding window remains in the range "+(str(win_begin+1))+" to "+str(win_end)+". Resend the same sequence number.")
+                time.sleep(1)
 
 # closes the socket and closes the TCP connection
 clientSocket.close()
--- TCPServer.py
+++ TCPServer.py
@@ -1,19 +1,44 @@
-from socket import *
-serverPort = 12000
+import socket
 
 # creates the server’s socket
-serverSocket = socket(AF_INET,SOCK_STREAM)
-
-# associate the server port number with server socket
-serverSocket.bind(('',serverPort))
-
-# the server listen for TCP connection requests from the client. 
-# The parameter specifies the maximum number of queued connections (at least 1).
+serverSocket = socket.socket(AF_INET,SOCK_STREAM)
+host = socket.gethostname()
+ip = socket.gethostbyname(host)
+PORT = 1234
+serverSocket.bind((host, PORT))
+print(host, "(", ip, ")")
+     
 serverSocket.listen(1)
-print('The server is ready to receive')
+print("Server is listening...")
 
 while True:
     connectionSocket, addr = serverSocket.accept()
     response = 'success' if connectionSocket.recv(1024).decode() == 'network' else 'failure'
     connectionSocket.send(response.encode())
+    print("Established connection from ", addr[0])
+
+    while True:
+
+        message=s.recv(1024).decode()
+        if(message=='exit'):
+            print("Program terminated.")
+            break
+        seq_num_range=pow(2,int(message))-1
+        win_begin=0
+        ack_message=""
+        message=""
+
+        while win_begin!=seq_num_range:
+            random_num=random.randint(0,9)
+            if(random_num==0):
+                ack_message="ACK Lost"
+                message = s.recv(1024).decode()
+                s.send(ack_message.encode())
+
+            else:
+                ack_message="ACK "+str(win_begin)
+                message = s.recv(1024).decode()
+                s.send(ack_message.encode())
+                win_begin=win_begin+1
+
     connectionSocket.close()
