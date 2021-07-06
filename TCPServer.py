9d1338633df232ac482c9c1ef4060030bb3ee5d9
shiyunlian <shiyunlian00@gmail.com>
2021-07-05 20:07:23 -0700

    update server codes

--- TCPServer.py
+++ TCPServer.py
@@ -8,37 +8,42 @@ PORT = 1234
 serverSocket.bind((host, PORT))
 print(host, "(", ip, ")")
      
-serverSocket.listen(1)
-print("Server is listening...")
+try:
+    serverSocket.listen(1)
+    print('Server is listening...')
+except:
+    print('Server is not listening...')
 
-while True:
+try:
     connectionSocket, addr = serverSocket.accept()
     response = 'success' if connectionSocket.recv(1024).decode() == 'network' else 'failure'
     connectionSocket.send(response.encode())
     print("Established connection from ", addr[0])
+except:
+    print("Connection failed")
 
-    while True:
+while True:
 
-        message=serverSocket.recv(1024).decode()
-        if(message=='exit'):
-            print("Program terminated.")
-            break
-        seq_num_range=pow(2,int(message))-1
-        win_begin=0
-        ack_message=""
-        message=""
+    message = connectionSocket.recv(1024).decode()
+    if(message == 'exit'):
+        print("Program terminated.")
+        break
+    seq_num_range = int(pow(2,int(message))-1)
+    win_begin = 0
+    ack_message = ""
+    message = ""
 
-        while win_begin!=seq_num_range:
-            random_num=random.randint(0,9)
-            if(random_num==0):
-                ack_message="ACK Lost"
-                message = serverSocket.recv(1024).decode()
-                serverSocket.send(ack_message.encode())
+    while win_begin <= seq_num_range:
+        random_num = random.randint(0,99)
+        if(random_num == 0):
+            ack_message ="ACK Lost"
+            message = connectionSocket.recv(1024).decode()
+            connectionSocket.send(ack_message.encode())
 
-            else:
-                ack_message="ACK "+str(win_begin)
-                message = serverSocket.recv(1024).decode()
-                serverSocket.send(ack_message.encode())
-                win_begin=win_begin+1
+        else:
+            ack_message = "ACK "+str(win_begin)
+            message = connectionSocket.recv(1024).decode()
+            connectionSocket.send(ack_message.encode())
+            win_begin=win_begin+1
 
-    connectionSocket.close()
+connectionSocket.close()
