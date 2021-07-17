74466eb636fb191b928b79e927a06b2361c706a4
shiyunlian <shiyunlian00@gmail.com>
2021-07-16 23:53:00 -0700

    update part_c code

--- Part_C.py
+++ Part_C.py
@@ -2,10 +2,26 @@ import socket
 PORT = 1234
 serverSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 serverSocket.bind(('',PORT))
-print('Server is ready to listen')
 
-try:
-    serverSocket.listen(10)
-    print('Server is listening...')
-except:
-    print('Server is not listening...')
+connection_num = 10
+count = 1
+# try to establish connection and send response back to client
+while connection_num > 0:
+
+    try:
+        serverSocket.listen(connection_num)
+        print('Server is listening...')
+        conn, address = serverSocket.accept()
+        print('Received connection', count)
+        response = 'Thank you' 
+        conn.send(response.encode())
+        print('Sent thank you message', count)
+        count += 1
+    except:
+        print('Server is not listening...')
+
+    connection_num -= 1
+
+# close the connection
+conn.close() 
+print('Connection closed.') 
