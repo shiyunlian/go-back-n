62edb86947da54c03355362e39e6795d527e6880
shiyunlian <shiyunlian00@gmail.com>
2021-08-31 16:02:32 -0700

    remove extra files

--- project/test.py
+++
@@ -1,7 +0,0 @@
-import socket, random, time
-import matplotlib.pyplot as plt
-
-# get the hostname and ip
-host = socket.gethostname()
-ip = socket.gethostbyname(host)
-print(host, "ip address: ", ip)
